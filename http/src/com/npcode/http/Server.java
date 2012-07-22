bf9a884b7b387d207f4ebf56072590e7eb46dd48
poksion <poksoin@gmail.com>
2012-07-22 11:29:48 +0900

    Add ant binary and http build environment

---
+++ http/src/com/npcode/http/Server.java
@@ -0,0 +1,12 @@
+package com.npcode.http;
+
+public class Server {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	    System.out.println("Hello HTTP Server");
+    }
+
+}
