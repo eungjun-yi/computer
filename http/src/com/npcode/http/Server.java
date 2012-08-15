fd677e63dfa47afdba45b98a86ef46d2d09d3fb1
poksion <poksion@gmail.com>
2012-08-16 02:33:31 +0900

    Add image load function

--- http/src/com/npcode/http/Server.java
+++ http/src/com/npcode/http/Server.java
@@ -2,108 +2,95 @@ package com.npcode.http;
 
 public class Server {
 
-    static public class RequestMessage {
-        String method;
-        String uri;
-        String version;
+	static public class RequestMessage {
+		String method;
+		String uri;
+		String version;
 
-        public void build(String buffer) {
-            String[] segments = buffer.split(" ");
-            method = segments[0];
-            uri = segments[1];
-            version = segments[2];
-        }
-    }
-    
-    static public class FileUtil {
-    	public static String getMimeType(java.io.File f){
-    		return new javax.activation.MimetypesFileTypeMap().getContentType(f);
-    	}
-    }
+		public void build(String buffer) {
+			String[] segments = buffer.split(" ");
+			method = segments[0];
+			uri = segments[1];
+			version = segments[2];
+		}
+	}
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        System.out.println("Starting HTTP Server ..");
-        java.io.File directory = new java.io.File(".");
-        System.out.println("Current Dir : " + directory.getAbsolutePath());
-        
-        eu.medsea.mimeutil.MimeUtil.registerMimeDetector("eu.medsea.mimeutil.detector.MagicMimeMimeDetector");
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Starting HTTP Server ..");
+		java.io.File directory = new java.io.File(".");
+		System.out.println("Current Dir : " + directory.getAbsolutePath());
 
-        try {
-            java.net.ServerSocket serverSocket = new java.net.ServerSocket(8888);
-            while (true) {
-                System.out.println("Waiting for acception ..");
+		eu.medsea.mimeutil.MimeUtil.registerMimeDetector("eu.medsea.mimeutil.detector.ExtensionMimeDetector");
 
-                java.net.Socket client = serverSocket.accept();
+		try {
+			java.net.ServerSocket serverSocket = new java.net.ServerSocket(8181);
+			while (true) {
+				System.out.println("Waiting for acception .. 0.03");
 
-                java.io.InputStream client_is = client.getInputStream();
-                java.io.OutputStream client_os = client.getOutputStream();
+				java.net.Socket client = serverSocket.accept();
 
-                java.io.BufferedReader bufReader = new java.io.BufferedReader(
-                        new java.io.InputStreamReader(client_is));
-                java.io.BufferedWriter bufWriter = new java.io.BufferedWriter(
-                        new java.io.OutputStreamWriter(client_os));
+				java.io.InputStream client_is = client.getInputStream();
+				java.io.OutputStream client_os = client.getOutputStream();
 
-                // read from client
-                StringBuilder received_total_buffer = new StringBuilder();
-                String received = bufReader.readLine();
-                while (received.length() > 0) {
-                    received_total_buffer.append(received + "\n");
-                    received = bufReader.readLine();
-                }
+				java.io.BufferedReader bufReader = new java.io.BufferedReader(
+						new java.io.InputStreamReader(client_is));
 
-                // TODO : parse from reading data
-                String received_total = received_total_buffer.toString();
-                RequestMessage requestMessage = new RequestMessage();
-                requestMessage.build(received_total);
+				// read from client
+				StringBuilder received_total_buffer = new StringBuilder();
+				String received = bufReader.readLine();
+				if(received== null){
+					client.close();
+					continue;
+				}
+				while (received.length() > 0) {
+					received_total_buffer.append(received + "\n");
+					received = bufReader.readLine();
+				}
 
-                String response = "";
-                if (requestMessage.method.equalsIgnoreCase("GET")) {
-                    java.io.File resource = new java.io.File("./htdocs" + requestMessage.uri);
-                    if (resource.exists()) {
-                    	java.util.Collection<?> mimeTypes = eu.medsea.mimeutil.MimeUtil.getMimeTypes(resource);
-                    	//String mimeType = FileUtil.getMimeType(resource);
-                    	System.out.println("Mime type is : " + mimeTypes);
-                    	
-                        java.io.FileInputStream fis = new java.io.FileInputStream(resource);
+				// TODO : parse from reading data
+				String received_total = received_total_buffer.toString();
+				RequestMessage requestMessage = new RequestMessage();
+				requestMessage.build(received_total);
 
-                        byte[] b = new byte[1024 * 1024];
-                        fis.read(b);
-                        
-                        String CRLF = "\r\n";
+				if (requestMessage.method.equalsIgnoreCase("GET")) {
+					java.io.File resource = new java.io.File("./htdocs" + requestMessage.uri);
+					if (resource.exists()) {
+						java.util.Collection<?> mimeTypes = eu.medsea.mimeutil.MimeUtil.getMimeTypes(resource);
+						System.out.println("Mime type is of (" + resource.getName() + ") : " + mimeTypes);
 
-                        // TODO : generate responding to client
-                        String content = new String(b);
-                        String message = "HTTP/1.1 200 OK" + CRLF;
-                        // message += "Date: Sun, 22 July 2012 17:30:00 GMT\n";
-                        message += "Server: npcode-http" + CRLF;
-                        message += "Content-Length: " + content.getBytes().length + CRLF;
-                        message += "Content-Type: " + mimeTypes + CRLF + CRLF;;
-                        message += content;
+						java.io.FileInputStream fis = new java.io.FileInputStream(resource);
 
-                        response = message;
-                    } else {
-                        response = "HTTP/1.1 404 Not Found\n\n";
-                    }
-                }
+						byte[] b = new byte[1024 * 1024];
+						int content_length = fis.read(b);
 
-                // write to client
-                // System.out.print("SERVER RECEIVED : \n" + received_total);
-                // bufWriter.write("SERVER SENT : \n" + received_total);
-                bufWriter.write(response);
-                bufWriter.flush();
+						String CRLF = "\r\n";
 
-                // close
-                bufReader.close();
-                bufWriter.close();
-                client.close();
-            }
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            System.out.println(e.toString());
-        }
-    }
+						// TODO : generate responding to client
+						String message = "HTTP/1.1 200 OK" + CRLF;
+						// message += "Date: Sun, 22 July 2012 17:30:00 GMT\n";
+						message += "Server: npcode-http" + CRLF;
+						message += "Content-Length: " + content_length + CRLF;
+						message += "Content-Type: " + mimeTypes + CRLF + CRLF;;
+						client_os.write(message.getBytes());
+						//client_os.flush();
+						client_os.write(b, 0, content_length);
+
+					} else {
+						client_os.write("HTTP/1.1 404 Not Found\n\n".getBytes());
+					}
+				}
+
+				// close
+				bufReader.close();
+				client.close();
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println(e.toString());
+		}
+	}
 }
